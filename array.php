d389581ed8073add8e942b15b3fb6ba333056e58
kafri bung <kafribung07@gmail.com>
2020-06-22 14:02:44 +0800

    mantap jiwa

---
+++ array.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Array</title>
+</head>
+<body>
+    <h1>Berlatih Array</h1>
+        
+    <?php 
+        echo "<h3> Soal 1 </h3>";
+        /* 
+            SOAL NO 1
+            Kelompokkan nama-nama di bawah ini ke dalam Array.
+            Kids : "Mike", "Dustin", "Will", "Lucas", "Max", "Eleven" 
+            Adults: "Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"
+        */
+        $kids = ["Mike", "Dustin", "Will", "Lucas", "Max", "Eleven"];
+        $adults = array("Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"); 
+
+        print_r(array_merge($kids, $adults));
+
+        echo "<h3> Soal 2</h3>";
+        /* 
+            SOAL NO 2
+            Tunjukkan panjang Array di Soal No 1 dan tampilkan isi dari masing-masing Array.
+        */
+        echo "Cast Stranger Things: ";
+        echo "<br>";
+        echo "Total Kids: " ; // Berapa panjang array kids
+        echo "<br>";
+        echo "<ol>"; 
+
+        foreach ($kids as $kid) {
+            echo "<li> $kid </li>";
+        }
+        // Lanjutkan
+
+        echo "</ol>";
+        
+        echo "Total Adults: " ;// Berapa panjang array adults
+        echo "<br>";
+        echo "<ol>";
+
+        foreach ($adults as $adult) {
+            echo "<li> $adult </li>";
+        }
+        // Lanjutkan
+
+        echo "</ol>";
+
+
+        echo "<h3> Soal 3</h3>";
+
+
+        /*
+            SOAL No 3
+            Susun data-data berikut ke dalam bentuk Asosiatif Array (Array Multidimensi)
+            
+            Name: "Will Byers"
+            Age: 12,
+            Aliases: "Will the Wise"
+            Status: "Alive"
+
+            Name: "Mike Wheeler"
+            Age: 12,
+            Aliases: "Dungeon Master"
+            Status: "Alive"
+
+            Name: "Jim Hopper"
+            Age: 43,
+            Aliases: "Chief Hopper"
+            Status: "Deceased"
+
+            Name: "Eleven"
+            Age: 12,
+            Aliases: "El"
+            Status: "Alive"
+            
+        */
+
+        $mahasiswas = [
+            [ 'Name' => "Will Byers",
+            'Age' => 12,
+            'Aliases' => "Will the Wise",
+            'Status' =>"Alive"],
+
+            [ 'Name' => "Mike Wheeler",
+            'Age' => 12,
+            'Aliases' => "Dungeon Master",
+            'Status' =>"Alive"],
+
+            [ 'Name' => "Jim Hopper",
+            'Age' => 43,
+            'Aliases' => "Chief Hopper",
+            'Status' =>"Deceased"],
+
+            [ 'Name' => "Eleven",
+            'Age' => 12,
+            'Aliases' => "El",
+            'Status' =>"Alive"],
+
+        ];
+
+        foreach ($mahasiswas as $mahasiswa) {
+                echo $mahasiswa['Name'] . "<br>";
+                echo $mahasiswa['Age'] . "<br>";
+                echo $mahasiswa['Aliases'] . "<br>";
+                echo $mahasiswa['Status'] . "<br>";
+                echo "<br>";
+        }        
+    ?>
+</body>
+</html>
